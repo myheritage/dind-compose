b07294543260b8dde21666d77ada7264be2350da
elad <elad@myheritage.com>
2017-05-10 14:29:08 +0300

    Dumb, but seems that Jenkins sometime overrides the entrypoint and sometime doesn't, so we need to verify we don't start docker twice

--- jenkins-docker-entrypoint.sh
+++ jenkins-docker-entrypoint.sh
@@ -3,22 +3,25 @@ set -e
 update-ca-trust force-enable
 update-ca-trust extract
 
-if [ "$LOG" == "file" ]
+if ! docker info >/dev/null 2>&1
 then
-	nohup dockerd -H 0.0.0.0:2375 -H unix:///var/run/docker.sock --storage-driver=vfs $DOCKER_DAEMON_ARGS &>/var/log/docker.log &
-else
-	nohup dockerd -H 0.0.0.0:2375 -H unix:///var/run/docker.sock --storage-driver=vfs $DOCKER_DAEMON_ARGS &
+  if [ "$LOG" == "file" ]
+  then
+  	nohup dockerd -H 0.0.0.0:2375 -H unix:///var/run/docker.sock --storage-driver=vfs $DOCKER_DAEMON_ARGS &>/var/log/docker.log &
+  else
+  	nohup dockerd -H 0.0.0.0:2375 -H unix:///var/run/docker.sock --storage-driver=vfs $DOCKER_DAEMON_ARGS &
+  fi
+  (( timeout = 60 + SECONDS ))
+  until docker info >/dev/null 2>&1
+  do
+  	if (( SECONDS >= timeout )); then
+  		echo 'Timed out trying to connect to internal docker host.' >&2
+      exit 1
+  		break
+  	fi
+  	sleep 1
+  done
 fi
-(( timeout = 60 + SECONDS ))
-until docker info >/dev/null 2>&1
-do
-	if (( SECONDS >= timeout )); then
-		echo 'Timed out trying to connect to internal docker host.' >&2
-    exit 1
-		break
-	fi
-	sleep 1
-done
 if [[ "$1" == "sh" ]]; then
   exec $@
 else
